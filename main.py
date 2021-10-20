842c08b90f7161dc9ac57c8bf7b692be17e7f2c8
eugene-h-kim <h_kim123224@fanshaweonline.ca>
2021-10-20 16:31:12 -0400

    player choices working

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-name = "Eugene"
-eyeColour = "dark brown"
-heightInFeet = 6
-hairColour = "black"
+from random import randint
 
-print("==========================================")
-print("My name is " + name + " and I have " + eyeColour + "eyes")
-print("I'm about " + str(heightInFeet) + " feet tall and have " + hairColour + "hair.")
-print("I look a bit like a Fraggle, actually.")
-print("==========================================")
+choices = ["rock", "paper", "scissors"]
+
+# player will be the weapon the player chooses via input
+player = input("Choose your weapon: rock, paper or scissors: ")
+
+computer = choices[randint(0, 2)]
+
+print("player chose: " + player)
+print("computer chose:" + computer)
